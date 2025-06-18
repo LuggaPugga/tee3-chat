9c88079652f6c38f8efe2e0059ac198c4f83e005
set <lucameopou@gmail.com>
2025-06-18 12:07:24 +0200

    fix: update environment variable for InstantDB app ID

--- src/utils/instant-admin.ts
+++ src/utils/instant-admin.ts
@@ -2,7 +2,7 @@ import { init } from "@instantdb/admin"
 import schema from "instant.schema"
 
 export const db = init({
-  appId: import.meta.env.VITE_INSTANT_DB_APP_ID,
+  appId: import.meta.env.VITE_INSTANT_APP_ID,
   adminToken: process.env.INSTANT_DB_ADMIN_TOKEN as string,
   schema: schema,
 })
