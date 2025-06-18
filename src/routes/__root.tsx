225ebad403e52b447010ff1bb92af7ac8c98d920
set <lucameopou@gmail.com>
2025-06-18 19:44:08 +0200

    chore: update SEO metadata for project rebranding to Tee3 Chat

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -22,8 +22,8 @@ export const Route = createRootRoute({
         content: "width=device-width, initial-scale=1",
       },
       ...seo({
-        title: "TanStack Start | Type-Safe, Client-First, Full-Stack React Framework",
-        description: `TanStack Start is a type-safe, client-first, full-stack React framework. `,
+        title: "Tee3 Chat",
+        description: `tee3.chat is a t3.chat clone.`,
       }),
     ],
     links: [
